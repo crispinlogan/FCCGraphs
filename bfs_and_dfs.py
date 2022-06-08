dccbf211cdb459cb91c47a2fe666cd38661d2a7d
Crispin Logan <crispin.logan@sony.com>
2022-06-08 17:16:57 +0100

    Add to bfs and dfs

--- bfs_and_dfs.py
+++ bfs_and_dfs.py
@@ -55,6 +55,18 @@ def dfs(node: str):
             if neighbour not in visited:
                 s.append(neighbour)
 
+def dfs(node):
+    """Recursive"""
+    visited = set()
+    def dfs_helper(node):
+        if node not in visited:
+            print(node)
+            visited.add(node)
+            for neighbour in graph[node]:
+                if neighbour not in visited:
+                    dfs_helper(neighbour)
+    dfs_helper(node)
+
 dfs('a')
 dfs('d')
 dfs('f')
@@ -79,6 +91,22 @@ def bfs(node: str):
         for neighbour in graph[curr]:
             if neighbour not in visited:
                 q.append(neighbour)
+
+def bfs(node: str):
+    """Compare with above BFS and see where the nodes are being marked as visited - this can actually be better...
+    you can also do the same with stack-based DFS - i.e. move where the nodes are being marked as visited
+    For info see: https://stackoverflow.com/questions/63321640/when-do-you-add-a-node-to-visited-in-bfs-or-dfs"""
+    visited = set()
+    q = deque()
+    q.append(node)
+    visited.add(node)
+    while q:
+        curr = q.popleft()
+        print(curr)
+        for neighbour in graph[curr]:
+            if neighbour not in visited:
+                q.append(neighbour)
+                visited.add(neighbour)
 
 bfs('a')
 bfs('d')
